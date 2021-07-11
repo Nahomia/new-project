1572a8c8d6f31474071ec11010ed1484657c2143
nardosa <85627385+nardosa@users.noreply.github.com>
2021-07-11 18:58:01 +0300

    Update Game.cpp
    I have initialized variables and written a code that let the user enter values

--- Game.cpp
+++ Game.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include<cstdlib>
+#include<ctime>
 using namespace std;
 
 int main ()
 {
+  int number,guessed,counter=0;
+	srand(time(0));
+	number=rand()%100 + 1;
+	
+	do{
+	cout<<"Guess a number between 1 and 100\n";
+	cin>>guessed;
+	
+
